6a0102b60824c0a4df180f89e5fa929b62da5254
Daniel Schauenberg <d@unwiredcouch.com>
2010-06-03 16:01:51 +0200

    add shortcut functions to __init__.py

--- thunkapi/__init__.py
+++ thunkapi/__init__.py
@@ -1,7 +1,19 @@
 # encoding: utf-8
 
-from thunkapi import *
+from thunkapi import Thunk
 
 __author__ = "Daniel Schauenberg"
 __version__ = "0.1.0"
 __license__ = "MIT"
+
+def check(uid):
+    """ shortcut function for checking thunks"""
+    return Thunk().check(uid)
+
+def create(name=None):
+    """ shortcut function for creating thunks """
+    return Thunk().create(name)
+
+def poke(uid, state, payload=None):
+    """shortcut function for poking thunks"""
+    return Thunk().poke(uid, state, payload)
